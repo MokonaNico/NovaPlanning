3f228751cdc662e079b0a5382f5a098900af2cfd
MokonaNico <mine-nico@hotmail.com>
2022-09-16 17:41:53 +0200

    clean up scrap

--- scrap.py
+++ scrap.py
@@ -11,59 +11,46 @@ import dateparser
 import json
 import time
 
-dict_cours = {
-    'Réseaux II' : 'Réseaux II',
-    'Crypto sécurité info': 'Cryptographie et sécurité des systèmes informatiques',
-    'Graphes et intelligence artifi' : 'Graphes et intelligence artificielle',
-    'Deep Learning for Natural Lang' : 'Deep Learning for Natural Language and Sequence Processing',
-    'AdvDeepLearn' : 'Advanced machine learning and deep learning',
-    'AdvMachLearn' : 'Advanced machine learning and deep learning',
-    'Méthodes formelles : intro' : 'Méthodes formelles pour la conception de systèmes : fondements',
-    'Méthodes formelles : Fondem.' : 'Méthodes formelles pour la conception de systèmes : fondements',
-    'Méth. formelles application' : 'Méthodes formelles pour la conception de systèmes : applications',
-    'Calcul.et complexité' : 'Calculabilité et complexité',
-    'ComputVisMachInt' : 'Computer Vision and Machine Intelligence',
-    'SignProces1' : 'Signal Processing'
-}
-liste_cours = dict_cours.keys()
+def move_to_start_position(driver):
+    # Go into "Formations" tab
+    formation = driver.find_element(By.XPATH, '//div[text()="Formations"]')
+    action = ActionChains(driver)
+    action.move_to_element(formation)
+    action.click()
+    action.perform()
 
-events = []
+    # Go into "Récapitulatif des cours" tab
+    recap = driver.find_element(By.XPATH, '//div[text()="Options"]')
+    action = ActionChains(driver)
+    action.move_to_element(recap)
+    action.move_by_offset(70,30)
+    action.click()
+    action.perform()
+
+    select = driver.find_element(By.XPATH, '//div[@class="ocb_cont as-input as-select "]')
+    action = ActionChains(driver)
+    action.move_to_element(select)
+    action.move_by_offset(5,5)
+    action.click()
+    action.perform()
+
+def move_down(driver,n,begin_enter=False):
+    action = ActionChains(driver)
+    if(begin_enter):
+        action.send_keys(Keys.ENTER)
+    for i in range(n):
+       action.send_keys(Keys.ARROW_DOWN)
+    action.send_keys(Keys.ENTER)
+    action.perform()
+
 
 options = Options()
-options.headless = False
-
+options.headless = True
 driver = webdriver.Firefox(options=options)
 driver.get("https://hplanning2022.umons.ac.be/invite")
-time.sleep(5)
-
-# Go into "Formations" tab
-formation = driver.find_element(By.XPATH, '//div[text()="Formations"]')
-action = ActionChains(driver)
-action.move_to_element(formation)
-action.click()
-action.perform()
-
-# Go into "Récapitulatif des cours" tab
-recap = driver.find_element(By.XPATH, '//div[text()="Options"]')
-action = ActionChains(driver)
-action.move_to_element(recap)
-action.move_by_offset(70,30)
-action.click()
-action.perform()
-
-select = driver.find_element(By.XPATH, '//div[@class="ocb_cont as-input as-select "]')
-action = ActionChains(driver)
-action.move_to_element(select)
-action.move_by_offset(5,5)
-action.click()
-action.pause(1)
-action.send_keys(Keys.ARROW_DOWN)
-action.send_keys(Keys.ARROW_DOWN)
-action.send_keys(Keys.ARROW_DOWN)
-action.perform()
-
-select = driver.find_element(By.XPATH, '//div[@class="ocb_cont as-input as-select "]')
-print(select.location)
-
-with open('events.json', 'w') as my_file:
-    my_file.writelines(json.dumps(events))
+time.sleep(5) # wait for the page to load
+move_to_start_position(driver)
+time.sleep(1)
+move_down(driver,16)
+time.sleep(1)
+print(driver.page_source)
