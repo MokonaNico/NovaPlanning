24f03eade42f1898eafa33a8595067066315049d
AdrienZianne <adrien.zianne@student.umons.ac.be>
2022-09-18 23:11:16 +0200

    v1

--- script/config.js
+++ script/config.js
@@ -19,6 +19,32 @@ function parse_event(events){
 		var table = document.createElement("table");
 		document.body.appendChild(table);
 
+		var div = document.createElement("div");
+		div.className = "checkbox-line";
+
+		var row = document.createElement("tr");
+		var checkbox_container = document.createElement("td");
+		var checkall = document.createElement("input");
+		checkall.type = "checkbox";
+		checkall.id = cursus;
+		checkall.onclick = onCheckAll;
+
+		var check_stored = JSON.parse(localStorage.getItem(cursus));
+		if(check_stored == null){
+			localStorage.setItem(cursus, false);
+			check_stored = false;
+		}
+		checkall.checked = check_stored;
+
+		var label = document.createElement("label");
+		label.innerHTML = "Tout sélectionner";
+
+		div.appendChild(checkall);
+		div.appendChild(label);
+		checkbox_container.appendChild(div);
+		row.appendChild(checkbox_container);
+		table.appendChild(row);
+
 		for (const [course, ignore] of Object.entries(courses)) {
 			var div = document.createElement("div");
 			div.className = "checkbox-line";
@@ -29,6 +55,7 @@ function parse_event(events){
 			checkbox_input.id = id;
 			checkbox_input.type = "checkbox";
 			checkbox_input.onclick = onCheck;
+			checkbox_input.name = cursus;
 
 			var check_stored = JSON.parse(localStorage.getItem(id));
 			if(check_stored == null){
@@ -40,6 +67,7 @@ function parse_event(events){
 			var label = document.createElement("label");
 			label.htmlFor  = id;
 			label.innerHTML = course;
+
 			div.appendChild(checkbox_input);
 			div.appendChild(label);
 			checkbox_container.appendChild(div);
@@ -49,6 +77,17 @@ function parse_event(events){
 	}
 }
 
+function onCheckAll(e) {
+	var isCheck = document.getElementById(e.target.id).checked;
+	checkboxes = document.getElementsByName(e.target.id);
+	for(const checkbox of checkboxes) {
+		checkbox.checked = isCheck;
+		localStorage.setItem(checkbox.id, isCheck);
+	}
+	localStorage.setItem(e.target.id, isCheck);
+}
+
 function onCheck(e) {        
 	localStorage.setItem(e.target.id,document.getElementById(e.target.id).checked);
+	console.log(document.getElementById(e.target.id).checked);
 }
