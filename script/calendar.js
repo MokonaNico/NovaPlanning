15c30102d4fe9e6a79fb8a69fe28118b9571e8ff
vandechat96 <vandechat96@gmail.com>
2022-09-21 20:52:32 +0200

    Deleted fullcalendar from local / events loaded from action branch / small cleanup / I hate webdev

--- script/calendar.js
+++ script/calendar.js
@@ -1,42 +1,42 @@
 !async function(){
-    await fetch('./events.json')
-        .then((response) => response.json())
-        .then(data => {
-            let events = []
-            for (const [cursus, courses] of Object.entries(data)) {
-                for (const [course, course_events] of Object.entries(courses)) {
-                    let stored = JSON.parse(localStorage.getItem(cursus+'_'+course));
-                    if (stored == null) continue;
-                    if (stored){
-                        course_events.forEach(function(item){
-                            item.title = item.title.replaceAll("\n","\n\n")
-                        });
-                        events = events.concat(course_events)
-                    }
+    fetch_events().then((events_fetch)=> {
+        let events = []
+        for (const [cursus, courses] of Object.entries(events_fetch)) {
+            for (const [course, course_events] of Object.entries(courses)) {
+                let stored = JSON.parse(localStorage.getItem(cursus+'_'+course));
+                if (stored == null) continue;
+                if (stored){
+                    course_events.forEach((item)=>{
+                        item.title = item.title.replaceAll("\n","\n\n");
+                        item.location = "test"
+                    });
+                    events = events.concat(course_events)
                 }
             }
-            calendar(events);
+        }
+        calendar(events);
 
-            for (let event of events) {
+        for (let event of events) {
 
-                let date = new Date(event['start'])
-                let start = [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(),date.getMinutes()]
-                date = new Date(event['end'])
-                let end = [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(),date.getMinutes()]
+            let date = new Date(event['start'])
+            let start = [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(),date.getMinutes()]
+            date = new Date(event['end'])
+            let end = [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(),date.getMinutes()]
 
-                let title = event['title']
+            let title = event['title']
 
-                let ics_event = {
-                    title,
-                    start,
-                    end,
-                }
-                ics_events.push(ics_event)
+            let ics_event = {
+                title,
+                start,
+                end,
             }
-        })
-        .catch(error => {
-            console.error(error);
-        });
+            ics_events.push(ics_event)
+        }
+    }).catch(error => {
+        console.error(error);
+    });
+
+
 }();
 
 function calendar(events) {
@@ -59,16 +59,14 @@ function calendar(events) {
             select: {
                 theme: 'true',
                 text: 'Sélection des cours',
-                click: function() {
+                click:() => {
                     location.href = "config.html";
                 }
             },
             ics: {
                 theme: 'true',
                 text: 'ICS',
-                click: function() {
-                    download();
-                }
+                click: () => download(),
             }
         },
         headerToolbar: {
--- script/ics.js
+++ script/ics.js
@@ -1,13 +1,13 @@
 import ics from 'https://cdn.skypack.dev/ics';
 import FileSaver from 'https://cdn.skypack.dev/file-saver';
-window.download = function (){
+window.download = () => {
     const { error, value } = ics.createEvents(ics_events)
 
     if (error) {
         console.log(error)
 
     } else {
-        let out = value.toString().replaceAll("\r\n\t","")
+        let out = value.toString().replaceAll("\r\n\n\t","")
         let blob = new Blob([out], {type: "text/plain;charset=utf-8"});
         FileSaver.saveAs(blob, "cours.ics");
     }
